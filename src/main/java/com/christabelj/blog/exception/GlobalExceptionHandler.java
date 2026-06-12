db1bd941180037b3729dbe52e05ba6ee2e156dc0
Christabel Johnson <christabel.a.johnson@accenture.com>
2026-06-12 11:28:29 +0100

    expanded method argument not valid to give information on what the error is

--- src/main/java/com/christabelj/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/christabelj/blog/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.List;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     // post not found (custom exception)
@@ -17,13 +19,18 @@ public class GlobalExceptionHandler {
 
     // invalid input - thrown by Spring
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
-        return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<String> errors = exception.getBindingResult() // .getBindingResult() — gets the validation result object
+                .getFieldErrors() // gets the list of fields that failed validation
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage()) // get the error field name and the message - making it readable
+                .toList();
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     // invalid uuid in the url - thrown by Spring
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception){
         return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
     }
 }
